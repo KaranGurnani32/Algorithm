985fc126ec75adb113d35927efbc0e56c97a9eeb
KaranGurnani32 <karang393@gmail.com>
2022-02-18 18:28:32 +0530

    Heap

--- Week2/src/Week2Questions/BinarySearch.java
+++ Week2/src/Week2Questions/BinarySearch.java
@@ -1,13 +1,13 @@
 package Week2Questions;
 
 public class BinarySearch {
-    public static void main(String[] args) {
-        int [] a = {1, 2, 3, 4, 88, 185, 390, 542};
-        int element = 542;
-        int location = binarySearchArray(a, element);
-        System.out.println(location);
-
-    }
+//    public static void main(String[] args) {
+//        int [] a = {1, 2, 3, 4, 88, 185, 390, 542};
+//        int element = 542;
+//        int location = binarySearchArray(a, element);
+//        System.out.println(location);
+//
+//    }
 
     private static int binarySearchArray(int[] a, int element) {
         int left = 0;
