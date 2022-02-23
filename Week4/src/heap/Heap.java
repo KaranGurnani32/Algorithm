3636bddec918f0f078aebe18d9e1a05a78cfd124
KaranGurnani32 <karang393@gmail.com>
2022-02-23 10:06:43 +0530

    heap

--- Week4/src/heap/Heap.java
+++ Week4/src/heap/Heap.java
@@ -40,7 +40,52 @@ public class Heap {
 
     public int remove() {
         if (this.data.length <= 0) {
-            System.out.println("Empty ");
+            System.out.println("Empty array");
+            return -404;
+        }
+
+        // remove the root node and this will give you either the max or min of a particular heap
+        int lastIndex = this.data.length - 1;
+
+        //swap the last element with the root(first) element of the heap
+        int elementToDelete = this.data[0];
+
+        int temp = this.data[0];
+        this.data[0] = this.data[lastIndex];
+        this.data[lastIndex] = temp;
+
+        //remove last index element
+        int [] newData = new int[lastIndex];
+        for (int i = 0; i < newData.length; i++) {
+            newData[i] = this.data[i];
+        }
+        this.data = newData;
+
+        heapCorrectionDownward();
+        return elementToDelete;
+    }
+
+    private void heapCorrectionDownward() {
+        int parentIndex = 0;
+        int leftChildIndex = (2 * parentIndex) + 1;
+        int rightChildIndex = (2 * parentIndex) + 2;
+
+        while(leftChildIndex < this.data.length && rightChildIndex < this.data.length) {
+
+            int currentLargerChildIndex = (this.data[leftChildIndex] > this.data[rightChildIndex]) ? leftChildIndex : rightChildIndex;
+
+            if(this.data[currentLargerChildIndex] > this.data[parentIndex]) {
+                int temp = this.data[parentIndex];
+                this.data[parentIndex] = this.data[currentLargerChildIndex];
+                this.data[currentLargerChildIndex] = temp;
+
+                //now make the new larger child the new parent and check the condition again
+                parentIndex = currentLargerChildIndex;
+                leftChildIndex = (2 * parentIndex) + 1;
+                rightChildIndex = (2 * parentIndex) + 2;
+            } else {
+                return;
+            }
         }
     }
 
