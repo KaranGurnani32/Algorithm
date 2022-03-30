340d8e38ddcd705aa342e60aff33f5cf15e58f43
KaranGurnani32 <karang393@gmail.com>
2022-03-30 10:15:17 +0530

    RemoveLastDuplicateEntries

--- Week5/src/binarysearchwebsiteques/RemoveLastDuplicateEntries.java
+++ Week5/src/binarysearchwebsiteques/RemoveLastDuplicateEntries.java
@@ -1,8 +1,6 @@
 package binarysearchwebsiteques;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class RemoveLastDuplicateEntries {
     public static void main(String[] args) {
@@ -13,18 +11,23 @@ public class RemoveLastDuplicateEntries {
 
     private static int[] solve(int[] nums) {
 
-        Set<Integer> set = new HashSet<>();
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            if (map.containsKey(nums[i]))
+                map.put(nums[i], i);
+            else
+                map.put(nums[i], -1);
 
-        for (int element : nums) {
-            set.add(element);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (map.get(nums[i]) != i)
+                list.add(nums[i]);
         }
 
-        int n = set.size();
-        int[] a = new int[n];
-        int i = 0;
-        for (int x : set) {
-            a[i++] = x;
-        }
-        return a;
+        int m = list.size();
+        int res[] = new int[m];
+        for (int i = 0; i < m; i++) res[i] = list.get(i);
+        return res;
     }
 }
