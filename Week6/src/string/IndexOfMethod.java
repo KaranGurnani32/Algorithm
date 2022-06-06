908df86f0196a0d3d4e9d9a20f85595b20609b68
KaranGurnani32 <karang393@gmail.com>
2022-06-06 11:15:52 +0530

    IndexOfMethod

---
+++ Week6/src/string/IndexOfMethod.java
@@ -0,0 +1,12 @@
+package string;
+
+public class IndexOfMethod {
+    public static void main(String[] args) {
+        String input = "flow";
+
+        // IndexOf method returns index of first occurrence of that character or string
+        System.out.println(input.indexOf("flo"));
+        // For lastIndex we apply lastIndexOf method
+        System.out.println(input.lastIndexOf("e"));
+    }
+}
