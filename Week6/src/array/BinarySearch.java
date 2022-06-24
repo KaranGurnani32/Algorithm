bc153d9e2477f10fdd82084d11870d4bdd606270
KaranGurnani32 <karang393@gmail.com>
2022-06-24 14:54:21 +0530

    BinarySearch

--- Week6/src/array/BinarySearch.java
+++ Week6/src/array/BinarySearch.java
@@ -4,7 +4,7 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] array = {-4, -1, 3, 7, 10, 11};
 
-        int key = 7;
+        int key = 2;
         System.out.println(binarySearch(array, key));
     }
 
@@ -23,6 +23,6 @@ public class BinarySearch {
                 low = mid + 1;
             }
         }
-        return -1;
+        return low;
     }
 }
