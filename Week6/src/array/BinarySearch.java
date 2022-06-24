da0b027f9af2ccb7b136d746ab06da028fee8bd0
KaranGurnani32 <karang393@gmail.com>
2022-06-24 10:15:01 +0530

    BinarySearch

---
+++ Week6/src/array/BinarySearch.java
@@ -0,0 +1,28 @@
+package array;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] array = {-4, -1, 3, 7, 10, 11};
+
+        int key = 7;
+        System.out.println(binarySearch(array, key));
+    }
+
+    private static int binarySearch(int[] array, int key) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (array[mid] == key) {
+                return mid;
+            } else if (array[mid] > key) {
+                high = mid - 1;
+            } else if (array[mid] < key){
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
